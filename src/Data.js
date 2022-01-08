63b4fe77880a17b0dfb7060808a3ec2e13c1853e
elir-mahad <mmahad280@gmail.com>
2022-01-07 21:06:19 -0500

    Add data object which contains general quotes

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export const data = {
+  general: [
+    {
+      id: "gen1",
+      tag: "general",
+      title: "Value of a book",
+      quoteText:
+        "A book can provide a context, of shared understanding and shared values, from which a powerful conversation can emerge.",
+    },
+    {
+      id: "gen2",
+      tag: "general",
+      title: "The world view #1",
+      quoteText:
+        "There are individual methods of spiritual growth, such as meditation and individual therapy, and then there are types of “mutual awakening”.",
+    },
+    {
+      id: "gen3",
+      tag: "general",
+      title: "The world view #2",
+      quoteText:
+        "We jump to higher levels of development not by excluding or making-wrong where we are now or where we were at before, but by including it.",
+    },
+    {
+      id: "gen4",
+      tag: "general",
+      title: "The world view #3",
+      quoteText:
+        "Within an external plan or form, the world can be co-created by us based on moment-to-moment experience. This is what the authentic relating games are. ",
+    },
+  ],
+};
