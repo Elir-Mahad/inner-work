d836d04f0257666bc3a1aedd4fd169d48bb37786
elir-mahad <mmahad280@gmail.com>
2022-01-13 22:37:45 -0500

    Create styled components in Styles.js for BasketHeartIcon component

---
+++ src/Styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+// BasketHeart
+
+export const BasketAndCountBox = styled.div`
+  //
+`;
+
+export const BasketAndCount = styled.div`
+  display: flex;
+  background-color: black;
+  color: white;
+`;
+
+export const BasketIcon = styled.div`
+  display: flex;
+  border: 1px solid white;
+  border-radius: 3px;
+  justify-content: center;
+  padding: 0.4rem;
+`;
+
+export const ItemCount = styled.p`
+  font-size: 1rem;
+  text-align: center;
+  position: relative;
+  top: 20px;
+  left: 35px;
+`;
