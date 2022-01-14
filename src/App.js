99feb1622ba70c7f4db7822b71d71f1705a3ce64
elir-mahad <mmahad280@gmail.com>
2022-01-13 22:03:00 -0500

    Add stateprovider, reducer, and local storage code to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+//
+import { StateProvider } from "./rca/StateProvider";
+import { initialState } from "./rca/reducer";
+import reducer from "./rca/reducer";
+//
+const basketFromLocalStorage = JSON.parse(
+  localStorage.getItem("basket") || "[]"
+);
 function App() {
+  //
+  const [basket, setBasket] = useState(basketFromLocalStorage);
+
+  useEffect(() => {
+    //
+    localStorage.setItem("basket", JSON.stringify(basket));
+    // Store the basket in the local storage
+  });
+  //
   return (
     <div className="App">
-      <h1>Inner work</h1>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <h1>Inner work</h1>
+      </StateProvider>
     </div>
   );
 }
