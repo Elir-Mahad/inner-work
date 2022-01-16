c484944f3ef88c45fb83fe09a3c9b3db62eabec7
elir-mahad <mmahad280@gmail.com>
2022-01-16 14:44:25 -0500

    Add react hooks and functions to QuoteContainer

--- src/components/containers/QuoteContainer.js
+++ src/components/containers/QuoteContainer.js
@@ -25,6 +25,54 @@ import { useStateValue } from "../../rca/StateProvider";
 
 function QuoteContainer({ id, tag, title, quoteText }) {
   // split apart the object, and grab the id, title, and quote text
+
+  const [{}, dispatch] = useStateValue();
+  // THIS IS THE DATA LAYER FROM STATE PROVIDER
+  // THIS IS USUALLY SUMMONED
+  // WHEN WILL NEED TO THE GRAB THE BASKET ITEMS
+  // OR THE WHOLE BASKET SOMEWHERE IN THE COMPONENT
+
+  //!Below is the function to add the item to the basket when the heart icon is clicked
+  const addToBasket = () => {
+    // console.log();
+    // add item to basket
+    dispatch({
+      type: "ADD_TO_BASKET", // this is the action.type from the reducer.js
+      item: {
+        id: id, // fetch the id of the item that's being added
+        tag: tag,
+        title: title,
+        quoteText: quoteText,
+      },
+    });
+  };
+
+  //! Below is the function to change the heart icon's color when its clicked
+  //! The color change only remains while the user is still on that category
+  //! If the category is changed the color of the heart icon, changes back to black
+  const [iconColor, setIconColor] = useState("black");
+  // The constant iconColor stores the value 'black'
+  // The useState declares that this value will be manipulated
+
+  const changeColor = () => {
+    // The constant changeColor stores a function
+    setIconColor("darkred");
+    // that changes the value of iconColor to 'darkred'
+  };
+
+  //! Below is the function that combines the two previous functions
+  const addToBaskeAndChangeColor = () => {
+    // The constant addToBaskeAndChangeColor stores
+    addToBasket();
+    // the function that adds a quote to the basket
+    changeColor();
+    // the function that changes heart color on click
+  };
+
+  // const nquoteText = quoteText.replace(/,/g, "\n").toString();
+
+  // console.log(nquoteText);
+
   return (
     <div>
       <h1>quote container</h1>
