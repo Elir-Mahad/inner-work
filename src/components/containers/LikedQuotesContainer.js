3a0d7dba54ddef596d1d02359040ae7c2eb73c04
elir-mahad <mmahad280@gmail.com>
2022-01-16 15:03:19 -0500

    Add imports to LikedQuotesContainer

--- src/components/containers/LikedQuotesContainer.js
+++ src/components/containers/LikedQuotesContainer.js
@@ -1,4 +1,27 @@
 import React from "react";
+import "../../App.css";
+import {
+  LiCategoryIcons,
+  LiQuoteBox,
+  LiQuotesRight,
+  LiQuotesLeft,
+  LiQuotetitle,
+  LiTag,
+  LiTextAndQuotes,
+  LiTextIcon,
+  LiTheQuote,
+  LiTopTextIcon,
+  LiBackgroundImageContainer,
+  LiIconButton,
+} from "../../Styles";
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
+//
+import SubjectIcon from "@material-ui/icons/Subject";
+import FormatListNumberedRtlIcon from "@material-ui/icons/FormatListNumberedRtl";
+import DeleteIcon from "@material-ui/icons/Delete";
+//
+import { useStateValue } from "../../rca/StateProvider";
 
 function LikedQuotesContainer() {
   return (
