c40e222aacf2319de45fe5db80ef090180e5ef63
elir-mahad <mmahad280@gmail.com>
2022-01-16 14:16:40 -0500

    Add imports to QuoteContainer

--- src/components/containers/QuoteContainer.js
+++ src/components/containers/QuoteContainer.js
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../App.css";
+import {
+  CategoryIcons,
+  QuoteBox,
+  QuotesRight,
+  QuotesLeft,
+  Quotetitle,
+  Tag,
+  TextAndQuotes,
+  TextIcon,
+  TheQuote,
+  TopTextIcon,
+  BackgroundImageContainer,
+  IconButton,
+} from "../../Styles";
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
+//
+import SubjectIcon from "@material-ui/icons/Subject";
+import FormatListNumberedRtlIcon from "@material-ui/icons/FormatListNumberedRtl";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+//
+import { useStateValue } from "../../rca/StateProvider";
 
 function QuoteContainer() {
   return (
