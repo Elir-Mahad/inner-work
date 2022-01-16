6cc86f6b9aa2a8300e2236bdb93a0342200a7990
elir-mahad <mmahad280@gmail.com>
2022-01-16 13:23:41 -0500

    Add imports to Menu

---
+++ src/components/general-parts/Menu.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useToggle } from "./usetoogle";
+import {
+  MenuBox,
+  MenuIconContainer,
+  MenuItems,
+  ItemAndIcon,
+  MenuItem,
+  MenuItemIcon,
+} from "../../Styles";
+import MenuIcon from "@material-ui/icons/Menu";
+//
+import logo from "../../assets/logo.jpeg";
+import description from "../../assets/description.png";
+import guide from "../../assets/info.png";
+import time from "../../assets/time.png";
+//
+function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+    </div>
+  );
+}
+
+export default Menu;
