a4ecd9b8fa610c5f4af0c3db973d3d726749e204
elir-mahad <mmahad280@gmail.com>
2022-01-16 15:07:46 -0500

    Add react hooks and functions to LikedQuotesContainer

--- src/components/containers/LikedQuotesContainer.js
+++ src/components/containers/LikedQuotesContainer.js
@@ -24,6 +24,23 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { useStateValue } from "../../rca/StateProvider";
 
 function LikedQuotesContainer({ id, tag, title, quoteText }) {
+  //
+  const [{}, dispatch] = useStateValue();
+  // THIS IS THE DATA LAYER FROM STATE PROVIDER
+  // THIS IS USUALLY SUMMONED
+  // WHEN WILL NEED TO THE GRAB THE BASKET ITEMS
+  // OR THE WHOLE BASKET SOMEWHERE IN THE COMPONENT
+
+  const removeFromBasket = () => {
+    // remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      // this is the action.type from the reducer.js
+      id: id,
+      // fetch the id of the item that's being removed
+    });
+  };
+
   return (
     <div>
       <h1>Liked quotes</h1>
