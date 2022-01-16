06b745c8fd9a8000473d68212a7b09d1bc853e2c
elir-mahad <mmahad280@gmail.com>
2022-01-16 13:42:11 -0500

    Add imports to MenuAndBasket

---
+++ src/components/general-parts/MenuAndBasket.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BasketHeartIcon from "./BasketHeartIcon";
+import { TopBar } from "../../Styles";
+import Menu from "./Menu";
+function MenuAndBasket() {
+  return (
+    <div>
+      <h1>Menu And basket</h1>
+    </div>
+  );
+}
+
+export default MenuAndBasket;
