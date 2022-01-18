715ad9d2fac4367a17dc710520654b9c7014087d
elir-mahad <mmahad280@gmail.com>
2022-01-17 20:58:30 -0500

    Import MenuAndBasket and Landing component into landing route in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //
+import MenuAndBasket from "./components/general-parts/MenuAndBasket";
+//
+import Landing from "./components/screens/LandingScreen";
+//
 import { StateProvider } from "./rca/StateProvider";
 import { initialState } from "./rca/reducer";
 import reducer from "./rca/reducer";
@@ -24,11 +28,14 @@ function App() {
       <StateProvider initialState={initialState} reducer={reducer}>
         <Router>
           <Switch>
-            <Route path="/">{/* Landing page route */}</Route>
-            <Route path="/synopsis">{/* Synopsis page route */}</Route>
-            <Route path="/userguide">{/* User guide page route */}</Route>
-            <Route path="/general">{/* General page route */}</Route>
             <Route path="/favourite">{/* Liked quotes route */}</Route>
+            <Route path="/general">{/* General page route */}</Route>
+            <Route path="/userguide">{/* User guide page route */}</Route>
+            <Route path="/synopsis">{/* Synopsis page route */}</Route>
+            <Route path="/">
+              <MenuAndBasket />
+              <Landing />
+            </Route>
           </Switch>
         </Router>
       </StateProvider>
