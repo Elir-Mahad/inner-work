92ba729af5ac610db649221de1f66cfb54160222
elir-mahad <mmahad280@gmail.com>
2022-01-17 19:50:54 -0500

    Add imports to SynopsisScreen

--- src/components/screens/SynopsisScreen.js
+++ src/components/screens/SynopsisScreen.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  SynopsisContainer,
+  DescriptionText,
+  SynopsisBackground,
+  SynopsisScreenImage,
+  SynopsisButton,
+} from "../../Styles";
+import mountainpath from "../../assets/synopsis-image1.jpeg";
+import pharoah from "../../assets/synopsis-image2.jpeg";
+//
 
 function SynopsisScreen() {
   return (
