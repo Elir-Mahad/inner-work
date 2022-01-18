1f54474345f02c10e71f5cd4182c1eb361031b57
elir-mahad <mmahad280@gmail.com>
2022-01-17 19:58:42 -0500

    Add imports to UserGuideScreen

---
+++ src/components/screens/UserGuideScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  UserGuideInstruction,
+  UserGuideContainer,
+  UserGuideBackground,
+  UserGuideScreenImage,
+  UserGuideNext,
+} from "../../Styles";
+import infoicon from "../../assets/info.png";
+
+function UserGuideScreen() {
+  return (
+    <div>
+      <h1>User guide</h1>
+    </div>
+  );
+}
+
+export default UserGuideScreen;
