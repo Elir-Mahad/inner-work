e0acdd72ea417075d7a0348fb7988ae3818c2f60
elir-mahad <mmahad280@gmail.com>
2022-01-17 20:09:20 -0500

    Create tagged template literals in Styles.js for GeneralScreen

--- src/Styles.js
+++ src/Styles.js
@@ -260,6 +260,15 @@ export const LiIconButton = styled.button`
   border: none;
 `;
 
+// Quotes styles for all other categories
+
+export const CategoryScreen = styled.div`
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`;
+
 // Landing
 
 export const LandingBackground = styled.div`
