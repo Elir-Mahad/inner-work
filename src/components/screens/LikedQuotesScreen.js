77591cee736ad868fff958c1531159896044caf2
elir-mahad <mmahad280@gmail.com>
2022-01-17 20:52:51 -0500

    Add tagged template literals to the return statement of LikedQuotesScreen

--- src/components/screens/LikedQuotesScreen.js
+++ src/components/screens/LikedQuotesScreen.js
@@ -19,9 +19,39 @@ function LikedQuotesScreen() {
   // OR THE WHOLE BASKET SOMEWHERE IN THE COMPONENT
   //
   return (
-    <div>
-      <h1>Liked quotes</h1>
-    </div>
+    <CategoryScreen>
+      <div>
+        {basket.length === 0 ? ( // if the basket is empty, has no items, then return the below div
+          <NoLikedQuotesContainer>
+            <NoLikedQuotesText>You have no Favourite quotes</NoLikedQuotesText>
+          </NoLikedQuotesContainer>
+        ) : (
+          // other wise (i.e, if the basket is not empty) return the below div
+
+          <div>
+            <LikedQuotesTitle> Your favourite quotes </LikedQuotesTitle>
+
+            {/* list all the checkout products */}
+
+            {/* for every single item, return the checkout products component */}
+
+            {basket.map((item, index) => (
+              <LikedQuotesContainer
+                //
+                key={index}
+                // adding this will stop this error:
+                // 'Encountered two children with the same key, ``. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.'
+                // More info here: programmersought.com/article/66582053289/
+                id={item.id}
+                tag={item.tag}
+                title={item.title}
+                quoteText={item.quoteText}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </CategoryScreen>
   );
 }
 
