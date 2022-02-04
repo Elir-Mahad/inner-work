b6d04407fc6cdc766302bb0d30aaf1f7bb526bb9
elir-mahad <mmahad280@gmail.com>
2022-02-04 16:07:55 -0500

    Remove margins for category screen

--- src/Styles.js
+++ src/Styles.js
@@ -328,9 +328,6 @@ export const LiIconButton = styled.button`
 // Quotes styles for all other categories
 
 export const CategoryScreen = styled.div`
-  margin-left: 1rem;
-  margin-right: 1rem;
-  margin-top: 2rem;
   margin-bottom: 2rem;
 `;
 // This styled component is used in all the category specific screen
