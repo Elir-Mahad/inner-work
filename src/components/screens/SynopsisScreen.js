643429c9929dc1a62989cfd8569c080d6898cc83
elir-mahad <mmahad280@gmail.com>
2022-02-04 15:52:42 -0500

    Add sidebar to synopsis screen

--- src/components/screens/SynopsisScreen.js
+++ src/components/screens/SynopsisScreen.js
@@ -9,37 +9,38 @@ import {
 } from "../../Styles";
 import mountainpath from "../../assets/synopsis-image1.jpeg";
 import pharoah from "../../assets/synopsis-image2.jpeg";
+import SideBar from "../general-parts/SideBar";
 //
 function Synopsis() {
   return (
     <SynopsisBackground>
-      <SynopsisScreenImage src={mountainpath} />
-      <SynopsisContainer>
-        <DescriptionText >
-          In noise,  <br />
-          there is no hearing. <br />
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <div id="page-wrap">
+        <SynopsisScreenImage src={mountainpath} />
+        <SynopsisContainer>
+          <DescriptionText>
+            In noise, <br />
+            there is no hearing. <br />
           </DescriptionText>
-          <DescriptionText >
-          With unfocused vision,  <br />
-          there is no sight. <br /> 
-          <DescriptionText >
+          <DescriptionText>
+            With unfocused vision, <br />
+            there is no sight. <br />
+            <DescriptionText></DescriptionText>
+            Without paitence, <br />
+            there is no understanding. <br />
           </DescriptionText>
-          Without paitence,  <br />
-          there is no understanding. <br />
-        </DescriptionText>
-      </SynopsisContainer>
-      <SynopsisScreenImage src={pharoah} />
-      <SynopsisContainer>
-          <DescriptionText >
-          Be the ship, <br />
-          that no wave can move.
-        </DescriptionText>
-        <Link style={{ textDecoration: "none" }} to="/userguide">
-          <SynopsisButton>
-          Learn more
-          </SynopsisButton>
+        </SynopsisContainer>
+        <SynopsisScreenImage src={pharoah} />
+        <SynopsisContainer>
+          <DescriptionText>
+            Be the ship, <br />
+            that no wave can move.
+          </DescriptionText>
+          <Link style={{ textDecoration: "none" }} to="/userguide">
+            <SynopsisButton>Learn more</SynopsisButton>
           </Link>
-      </SynopsisContainer>
+        </SynopsisContainer>
+      </div>
     </SynopsisBackground>
   );
 }
