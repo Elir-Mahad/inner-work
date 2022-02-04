afec790136aff291b1092df0d32f0e495e4bf5f8
elir-mahad <mmahad280@gmail.com>
2022-02-04 15:48:57 -0500

    Add react-router links to sidebar

--- src/components/general-parts/SideBar.js
+++ src/components/general-parts/SideBar.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 //
 function SideBar(props) {
   //
   return (
     <Menu {...props}>
-      {/*  */}
-      {/* <Link to="/"> */}
-      <p>Home</p>
-      {/* </Link> */}
-      {/* <Link> */}
-      <p>Synopsis</p>
-      {/* </Link> */}
-      {/* <Link> */}
-      <p>User guide</p>
-      {/* </Link> */}
-      {/* <Link> */}
-      <p>General quotes</p>
-      {/* </Link> */}
-      <p>Liked quotes</p>
-      {/*  */}
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      <Link to="/synopsis">
+        <p>Synopsis</p>
+      </Link>
+      <Link to="/userguide">
+        <p>User guide</p>
+      </Link>
+      <Link to="/general">
+        <p>General quotes</p>
+      </Link>
+      <Link to="/favourite">
+        <p>Liked quotes</p>
+      </Link>
     </Menu>
   );
 }
